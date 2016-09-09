8beb052410fb060b35cda150d3201d0786cfc007
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-09-08 21:07:03 -0700

    fixup boost packages for Travis/Docker scripts

--- .docker-build.sh
+++ .docker-build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 set -e
-yum install -y gmp-devel gcc boost #automake autoconf libtool
-mkdir build
-cd build
-/src/configure --srcdir=/src && make -j2 && make check
+yum install -y gmp-devel gcc gcc-c++ boost boost-devel boost-system boost-thread #automake autoconf libtool
+mkdir -p /build
+cd /build
+/src/configure --srcdir=/src --enable-plrs  || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
+make -j2 && make check

8ac88efd8ee2ca2154962ddfd189c2a0be242117
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-09-08 21:13:05 -0700

    fixup

--- .docker-build.sh
+++ .docker-build.sh
@@ -3,5 +3,5 @@ set -e
 yum install -y gmp-devel gcc gcc-c++ boost boost-devel boost-system boost-thread #automake autoconf libtool
 mkdir -p /build
 cd /build
-/src/configure --srcdir=/src --enable-plrs  || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
+/src/configure --srcdir=/src  || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
 make -j2 && make check

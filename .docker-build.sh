87e8f6f7538cc483ed581351e0b7ecf06f39ff34
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-08-19 17:57:07 +0200

    Travis/Docker: Switch to i386/ubuntu

--- .docker-build.sh
+++ .docker-build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
-yum install -y gmp-devel gcc gcc-c++ boost boost-devel boost-system boost-thread #automake autoconf libtool
+yum install -y gmp-devel gcc gcc-c++ boost boost-devel boost-system boost-thread || ( apt-get update && apt-get -y install libgmp-dev libboost-dev libboost-system-dev libboost-thread-dev autoconf automake libtool make)
 mkdir -p /build
 cd /build
 /src/configure --srcdir=/src  || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)

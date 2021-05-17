c0ac29e5cbf884beeee0bddc5f50d5909b8abaa0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-05-17 12:52:33 -0700

    .docker-build.sh, .travis.yml: Remove boost/plrs

--- .docker-build.sh
+++ .docker-build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -e
-yum install -y gmp-devel gcc gcc-c++ boost boost-devel boost-system boost-thread || ( apt-get update && apt-get -y install g++ libgmp-dev libboost-dev libboost-system-dev libboost-thread-dev autoconf automake libtool make)
+yum install -y gmp-devel gcc gcc-c++  || ( apt-get update && apt-get -y install g++ libgmp-dev autoconf automake libtool make)
 mkdir -p /build
 cd /build
-/src/configure --srcdir=/src --enable-plrs || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
+/src/configure --srcdir=/src || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
 make -j2 && make check
